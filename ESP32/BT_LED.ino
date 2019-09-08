8b890f4c657c840812886cc80b0ee510451a0770
dnjswp12 <49463724+dnjswp12@users.noreply.github.com>
2019-09-08 21:36:22 +0900

    Create BT_LED.ino
    classic BT

---
+++ ESP32/BT_LED.ino
@@ -0,0 +1,48 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  
+  pinMode (0, OUTPUT);
+  
+}
+
+void loop() {
+  
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    //Serial.write(SerialBT.read());
+    int a = SerialBT.read();
+    Serial.println(a);
+  
+    if (a == 1)
+  {
+    Serial.print("a is ");
+    Serial.println(a);
+    digitalWrite(0, HIGH);
+  }
+     if (a == 0)
+  {
+    Serial.print("a is ");
+    Serial.println(a);
+    digitalWrite(0, LOW);
+  }
+  }
+}
+
