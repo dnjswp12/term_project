39d54e20ea2ff1b2f21a9c70cde96f20729266b6
dnjswp12 <49463724+dnjswp12@users.noreply.github.com>
2019-09-23 17:28:44 +0900

    Create UART0.h

---
+++ Atmega128/UART/UART0.h
@@ -0,0 +1,101 @@
+typedef unsigned char  INT8;
+typedef unsigned int   INT16;
+
+#define sbi(reg,bit)    reg |= (1<<(bit))	  // Set "bit"th bit of Register "reg"
+#define cbi(reg,bit)    reg &= ~(1<<(bit))
+
+#define BAUD 9600
+#define U2X_S 2 //U2X --> 1 or 2
+#define MYUBRR ((F_CPU*U2X_S)/(16L*BAUD)-1)
+
+void USART0_init(INT16 ubrr)
+{
+	// Set baud rate
+	UBRR0H=(INT8)(ubrr>>8);
+	UBRR0L=(INT8)ubrr;       
+	// Enable U2X
+	if(U2X_S==2)
+		sbi(UCSR0A,U2X);
+	else
+		cbi(UCSR0A,U2X);
+		
+	//Enable Tx & Rx Port
+	sbi(UCSR0B,RXEN); // RX핀 사용 PE0
+	sbi(UCSR0B,TXEN); // TX핀 사용 PE1
+	
+	//Set frame format : default값으로 세팅되어 있음
+	/*
+	cbi(UCSR0C,UMSEL); // 0: 비동기 1: 동기
+	cbi(UCSR0C,USBS); // 스톱비트 0: 1bit 1: 2bit
+	cbi(UCSR0C,UPM01); // NO parity 패러티 설정
+	cbi(UCSR0C,UPM00);
+	cbi(UCSR0B,UCSZ02);//8bit 데이터 비트 설정
+	sbi(UCSR0C,UCSZ01);
+	sbi(UCSR0C,UCSZ00);
+	*/
+	
+	//Enable interrupt
+	//sbi(UCSR0B,RXCIE); //RX
+	//sbi(UCSR0B,TXCIE); //TX
+	//sbi(UCSR0B,UDRIE); //UDR Empty
+	//sei(); // all Interrup EN
+}
+
+
+void USART0_Transmit(char data)
+{
+	//wait for empty transmit buffer
+	while (!(UCSR0A & 0x20)) // UDRE bit -> data resist empty : 1
+	{
+		;
+	}
+	//Put "data" into buffer,transmit the data
+	UDR0 = data;
+}
+
+void USART0_Transmit_NewLine(void)
+{
+	USART0_Transmit(0x0A); // LF
+	USART0_Transmit(0x0D); // CR
+}
+
+void USART0_Transmit_String(char *str)
+{
+	while(*str != '\0')
+	{
+		if(*str == '\n')
+		USART0_Transmit_NewLine();
+		else
+		USART0_Transmit(*str);
+		str++;
+	}
+}
+
+char USART0_Receive(void)
+{
+	//Wait for data to be received
+	while(!(UCSR0A&0x80)) //RXC bit
+	{
+		;	
+	}
+	//Get "data" from buffer, return the data
+	return UDR0;	
+}
+void rx0_string(char *str)
+{
+	int i=0;
+	while ((str[i] = USART0_Receive()) != '\n')
+	{
+		i ++;
+	}
+	str[i] = '\0';
+}
+void tx0_string(char *str)
+{
+	int i=0;
+	while (str[i] != '\0')
+	{
+		USART0_Transmit(str[i]);
+		i++;
+	}
+}
